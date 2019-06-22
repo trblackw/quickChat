cb7f4847f4d762ea4b6e90fd3c87635a4ce9d8e8
Tucker <trblackw@ncsu.edu>
2019-06-22 13:25:38 -0400

    remove failed attempt at proxying

--- server/index.ts
+++ server/index.ts
@@ -3,9 +3,7 @@ const path = require('path');
 const morgan = require('morgan');
 const cors = require('cors');
 const { json, urlencoded } = require('body-parser');
-const proxy = require('http-proxy-middleware');
 const routes = require('./routes');
-const Bundler = require('parcel-bundler');
 const app = express();
 
 //config
@@ -13,8 +11,6 @@ app.use(morgan('dev'));
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'client/public')));
-app.use('/api', proxy({ target: 'http://localhost:8080' }));
 app.use(routes);
 
 
