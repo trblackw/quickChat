9fbe3e70cc41d5af41208bb0fa4cbdb66f550eb9
Tucker <trblackw@ncsu.edu>
2019-06-24 09:03:37 -0400

    incorporate another socket via context

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,22 +1,23 @@
 import React, { useContext, Fragment } from 'react';
 import { render } from 'react-dom';
 import { APIService } from './FetchWrapper';
-import { SocketProvider, SocketContext } from './context';
+import { SocketProvider, MainSocket, AdminSocket } from './context';
 import ChatBox from './components/ChatBox';
 import { Router } from '@reach/router';
 import SideNav from './components/NavBar';
 
 const App: React.FC = (): JSX.Element => {
-   const socket = useContext(SocketContext);
+   const mainSocket = useContext(MainSocket.context);
+   const adminSocket = useContext(AdminSocket.context)
    const getStuff = async () => {
       const test = await new APIService().init('api/test');
       let json = await test.json();
    };
    return (
       <Fragment>
-            <SideNav />
+         <SideNav />
          <Router>
-            <ChatBox socket={socket} path='/' />
+            <ChatBox socket={mainSocket} path='/' />
          </Router>
       </Fragment>
    );
@@ -25,7 +26,7 @@ const App: React.FC = (): JSX.Element => {
 export default App;
 
 render(
-   <SocketProvider>
+   <SocketProvider client={MainSocket.client} context={MainSocket.context}>
       <App />
    </SocketProvider>,
    document.getElementById('root')
