2f7c31bcd530ab9b41a4f5fa5c6c7a2daf136013
Tucker <trblackw@ncsu.edu>
2019-06-25 17:00:01 -0400

    complete basic server setup

--- server/index.ts
+++ server/index.ts
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const cors = require('cors');
 const { json, urlencoded } = require('body-parser');
 const routes = require('./routes');
+const http = require('http');
 const app = express();
 
 //config
@@ -11,10 +12,32 @@ app.use(morgan('dev'));
 app.use(urlencoded({ extended: true }));
 app.use(json());
 app.use(cors());
-app.use(routes);
+app.use('/api', routes);
 
+const port = process.env.PORT || 8080;
+const server = http.createServer(app);
 
-const port = process.env.PORT || 8080
-app.listen(port, (): void => {
+//set up socket.io
+const io = require('socket.io')(server);
+
+// constants
+const CONNECTION: 'connection' | 'connect' = 'connection';
+const PING = 'ping';
+const PONG = 'pong';
+const newMessageToServer = 'newMessageToServer';
+const newMessageFromServer = 'newMessageFromServer';
+const emitMessageToConnections = 'emitMessageToConnections';
+
+io.on(CONNECTION, (socket: any): void => {
+   // socket.emit(newMessageFromServer, { message: `hey from server, here's the socket.id: ${socket.id}` });
+   socket.on(newMessageToServer, ({ message }: any): void => {
+      io.emit(emitMessageToConnections, message);
+   });
+});
+
+io.on(PING, () => console.log('PING FROM SERVER'));
+io.on(PONG, () => console.log('PONG FROM SERVER'));
+
+server.listen(port, (): void => {
    console.log(`server running on port ${port}`);
 });
